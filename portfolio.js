29fa101047cc1aafc7e152e73cf6ce3d57ec57af
Jesse <99325358+jruiz64@users.noreply.github.com>
2022-05-24 20:48:37 -0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,6 @@
+var projects = document.getElementById("projects");
+
+projects.addEventListener('click', function() {
+  $(projects).toggleClass("active");
+  $(".parent:not(#projects)").toggleClass("invisible");
+}, false);
